afa91b07a8a876bba39ff22bee3bc2862c785bb1
Wong <1123239392@qq.com>
2022-02-24 14:19:40 +0800

    完成了理解lodash中防抖与节流的源码复现

--- 防抖与节流.js
+++ 防抖与节流.js
@@ -43,42 +43,79 @@ function debounce(func, wait, options) {
     trailing = "trailing" in options ? !!options.trailing : trailing;
     //  执行用户传入的函数
     function invokeFunc(time) {
-      // ......
+      var args = lastArgs, 
+          thisArg = lastThis;
+      lastArgs = lastThis = undefined;
+      lastInvokeTime = time;
+      result = func.apply(thisArg, args);
+      return result;
     }
 
     //  防抖开始时执行的操作
     function leadingEdge(time) {
-      // ......
+      lastInvokeTime = time;
+      // 设置定时器，保证wait接下来的执行
+      timerId = setTimeout(timerExpired, wait);
+      // 如果判断leading是true的话，立即执行传入的函数
+      return leading ?  invokeFunc(time) : result;
     }
 
     //  计算仍然需要等待的时间
     function remainingWait(time) {
-      // ......
+      var timeSinceLastCall = time - lastCallTime;
+          timeSinceLastInvoke = time - lastInvokeTime;
+          timeWaiting =  wait - timeSinceLastCall;
+      // 判断是否存在最大等待时间，如果存在返回，需等待的时间到最后一次调用debounce 和 最大等待时间到上一次执行用户传入函数的时间的最小值
+      return maxing ? nativeMin(timeWaiting, maxWait - timeSinceLastInvoke) : timeWaiting;
     }
 
     //  判断此时是否应该执行用户传入的函数
     function shouldInvoke(time) {
-      // ......
+      var timeSinceLastCall = time - lastCallTime,
+          timeSinceLastInvoke = time - lastInvokeTime;
+          return (
+            latCallTime === undefined || // 首次执行没有lastCallTime
+            (timeSinceLastCall >= wait) || // 距离上次调用的时间已经大于设置的wait值
+            (timeSinceLastCall < 0) || // 不知道为什么会出现这种情况，可能调整了系统时间？
+            (maxing && timeSinceLastInvoke >= maxWait) // 设置了最大等待时间，并且等待时间超过了最大等待时间
+          );
     }
 
-    //  等待时间结束后的操作
+    //  等待时间结束后的操作,判断是否符合执行条件，符合的话触发trailingEdge，否则就需要计算需要等待的时间，并重新掉用这个函数
+    // 防抖的核心所在
     function timerExpired() {
-      // ......
+      var time = now();
+      // 如果符合调用函数的时间，就触发trailingEdge函数
+      if (shouldInvoke(time)) {
+        return trailingEdge(time);
+      }
+      // 不满足时间的话，需要重新计算需要等待的时间，然后再重新调用自己
+      timerId = setTimeout(timerExpired, remainingWait(time));
     }
 
     //  执行用户传入的函数
     function trailingEdge(time) {
-      // ......
+      timerId = undefined;
+      // 如前面如果存在lastArgs代表已经至少执行过一次debounce
+      if (trailing && lastArgs) {
+        return invokeFunc(time);
+      }
+      lastArgs = lastThis = undefined;
+      return result;
     }
 
     //  取消防抖
     function cancel() {
-      // ......
+      if (timerId !== undefined) {
+        clearTimeout(timerId);
+      }
+      lastInvokeTime = 0;
+      lastArgs = lastCallTime = lastThis = timerId = undefined;
     }
 
     //  立即执行用户传入的函数
     function flush() {
-      // ......
+      return timerId === undefined ? result : trailingEdge(now());
     }
 
     // 防抖开始的入口
@@ -93,6 +130,7 @@ function debounce(func, wait, options) {
           if(timerId === undefined) {
               return leadingEdge(lastCallTime);
           }
+          // 如果设置了最大等待时间，则立即执行用户传入的函数
           if (maxing) {
               timerId = setTimeout(timerExpired, wait);
               return invokeFunc(lastCallTime);
@@ -110,6 +148,10 @@ function debounce(func, wait, options) {
   }
 }
 
+
+
+
+
 // 节流（throttle）
 /*
     在函数执行一次之后， 该函数在指定时间期限内不再工作，直至过了这段时间才重新生效
@@ -126,4 +168,22 @@ function throttle(fn, delay) {
       valid = true;
     }, delay);
   };
+}
+
+// 再lodash中节流的原理和防抖一样，只是触发条件不同，节流其实就是maxWait为wait的防抖
+function throttle(func, wait, options) {
+  var leading = true;
+      trailing = true;
+  if (typeof func != "function") {
+    throw new TypeError(FUNC_ERROR);
+  }
+  if (isObject(options)) {
+    leading = 'leading' in options ? !!options.leading : leading;
+    trailing = 'trailing' in options ? !!options.trailing : trailing;
+  }
+  return debounce(func, wait, {
+    'leading': leading,
+    'maxWait': wait,
+    'trailing': trailing
+  });
 }
